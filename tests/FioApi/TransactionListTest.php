d2c0dbaaadd493def2eac7ef7b396b9698a41781
Martin Hujer <mhujer@gmail.com>
2015-04-17 12:59:43 +0200

    Added test for TransactionList class

---
+++ tests/FioApi/TransactionListTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FioApi;
+
+class TransactionListTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTransactionListValuesAreProperlySet()
+    {
+        $transactionList = json_decode(file_get_contents(__DIR__ . '/data/example-response.json'));
+
+        $transactionList = TransactionList::create($transactionList->accountStatement);
+
+        $this->assertSame(500, $transactionList->getOpeningBalance());
+        $this->assertSame(1000, $transactionList->getClosingBalance());
+        $this->assertEquals(new \DateTime('2015-03-30'), $transactionList->getDateStart());
+        $this->assertEquals(new \DateTime('2015-03-31'), $transactionList->getDateEnd());
+        $this->assertSame(1111111111, $transactionList->getIdFrom());
+        $this->assertSame(1111111999, $transactionList->getIdTo());
+        $this->assertSame(null, $transactionList->getIdLastDownload());
+        $this->assertInstanceOf('FioApi\Account', $transactionList->getAccount());
+        $this->assertInstanceOf('FioApi\Transaction', $transactionList->getTransactions()[0]);
+    }
+}
