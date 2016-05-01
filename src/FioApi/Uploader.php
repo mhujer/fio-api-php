98985dc39cdea63b960b7db37f1338f9f293c845
Petr Kramar <kramar@masquerade.cz>
2016-05-01 18:22:29 +0200

    Add Uploader to import new transaction to process
    - Separate shared codebase of Downloader and Uploader into
      AbstractClient
    - Update documentation

---
+++ src/FioApi/Uploader.php
@@ -0,0 +1,51 @@
+<?php
+namespace FioApi;
+
+/**
+ * Uploader for Fio bank
+ *
+ * @link https://perlur.cz/
+ * @author Petr Kramar <petr.kramar@perlur.cz>
+ */
+class Uploader extends AbstractClient
+{
+    const URL = 'https://www.fio.cz/ib_api/rest/import/';
+
+    /**
+     * Send request
+     *
+     * @param string $request XML formatted request
+     * @return ImportResponse
+     * @throws BadResponseException
+     */
+    public function sendRequest($request)
+    {
+        $client = $this->getClient();
+        try {
+            $response = $client->request('post', self::URL, [
+                'verify' => $this->getCertificatePath(),
+                'multipart' => [
+                    [ 'name' => 'token', 'contents' => $this->getToken() ],
+                    [ 'name' => 'type', 'contents' => 'xml' ],
+                    [ 'name' => 'file', 'contents' => $request, 'filename' => 'request.xml' ],
+                    [ 'name' => 'lng', 'contents' => 'en' ]
+                ]
+            ]);
+
+            return $this->createResponse((string) $response->getBody());
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Create instance of ImportReponse
+     *
+     * @param string $body
+     * @return ImportResponse
+     */
+    protected function createResponse($body)
+    {
+        return new ImportResponse($body);
+    }
+}
---
+++ tests/FioApi/UploaderTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace FioApi;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
+
+class UploaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSendRequest()
+    {
+        $request = file_get_contents(__DIR__ . '/data/01-valid-request.xml');
+        $handler = HandlerStack::create(new MockHandler([
+           new Response(200, [], file_get_contents(__DIR__ . '/data/01-valid-response.xml'))
+        ]));
+        $client = new Client(['handler' => $handler]);
+        $uploader = new Uploader('token', $client);
+
+        $response = $uploader->sendRequest($request);
+        $this->assertInstanceOf('\FioApi\ImportResponse', $response);
+    }
+}
