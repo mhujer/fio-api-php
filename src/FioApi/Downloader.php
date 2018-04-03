519d5935a9d51ad8070fe83b80ecd983619c4d7c
Jiří Pudil <me@jiripudil.cz>
2018-04-03 13:54:43 +0200

    Support /last and /set-last-id endpoints

--- src/FioApi/Downloader.php
+++ src/FioApi/Downloader.php
@@ -58,31 +58,59 @@ class Downloader
 
     public function downloadFromTo(\DateTimeInterface $from, \DateTimeInterface $to): TransactionList
     {
-        $client = $this->getClient();
         $url = $this->urlBuilder->buildPeriodsUrl($from, $to);
-
-        try {
-            /** @var ResponseInterface $response */
-            $response = $client->request('get', $url, ['verify' => $this->getCertificatePath()]);
-        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-            if ($e->getCode() == 409) {
-                throw new TooGreedyException('You can use one token for API call every 30 seconds', $e->getCode(), $e);
-            }
-            if ($e->getCode() == 500) {
-                throw new InternalErrorException(
-                    'Server returned 500 Internal Error (probably invalid token?)',
-                    $e->getCode(),
-                    $e
-                );
-            }
-            throw $e;
-        }
-
-        return TransactionList::create(json_decode($response->getBody()->getContents())->accountStatement);
+        return $this->downloadTransactionsList($url);
     }
 
     public function downloadSince(\DateTimeInterface $since): TransactionList
     {
         return $this->downloadFromTo($since, new \DateTimeImmutable());
+    }
+
+    public function downloadLast(): TransactionList
+    {
+        $url = $this->urlBuilder->buildLastUrl();
+        return $this->downloadTransactionsList($url);
+    }
+
+    public function setLastId(string $id): void
+    {
+        $client = $this->getClient();
+        $url = $this->urlBuilder->buildSetLastIdUrl($id);
+
+        try {
+            $client->request('get', $url, ['verify' => $this->getCertificatePath()]);
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            $this->handleException($e);
+        }
+    }
+
+    private function downloadTransactionsList(string $url): TransactionList
+    {
+        $client = $this->getClient();
+
+        try {
+            /** @var ResponseInterface $response */
+            $response = $client->request('get', $url, ['verify' => $this->getCertificatePath()]);
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            $this->handleException($e);
+        }
+
+        return TransactionList::create(json_decode($response->getBody()->getContents())->accountStatement);
+    }
+
+    private function handleException(\GuzzleHttp\Exception\BadResponseException $e): void
+    {
+        if ($e->getCode() == 409) {
+            throw new TooGreedyException('You can use one token for API call every 30 seconds', $e->getCode(), $e);
+        }
+        if ($e->getCode() == 500) {
+            throw new InternalErrorException(
+                'Server returned 500 Internal Error (probably invalid token?)',
+                $e->getCode(),
+                $e
+            );
+        }
+        throw $e;
     }
 }
